c03696d290ec22d181af3d97b6cf3ca15bf882d5
Louis.S <louis.sasse@protonmail.com>
2024-04-12 16:23:00 +0200

    fix: change sqlite connection to pg connection

--- src/db.rs
+++ src/db.rs
@@ -2,15 +2,16 @@ pub mod models;
 pub mod schema;
 
 use actix_web::web;
+use diesel::OptionalExtension;
 use diesel::{
     r2d2::{self, ConnectionManager},
     result::Error,
-    ExpressionMethods, QueryDsl, RunQueryDsl, SqliteConnection,
+    ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl,
 };
 
 use crate::port::Port;
 
-pub type DatabaseConnection = SqliteConnection;
+pub type DatabaseConnection = PgConnection;
 pub type Pool = r2d2::Pool<ConnectionManager<DatabaseConnection>>;
 
 pub fn get_used_ports(pool: web::Data<Pool>) -> Result<Vec<u32>, Error> {
@@ -19,22 +20,29 @@ pub fn get_used_ports(pool: web::Data<Pool>) -> Result<Vec<u32>, Error> {
         .select(schema::ports::port)
         .load::<i32>(&mut conn)
         .unwrap();
-    // let ports_form_server:
     Ok(ports_from_db.into_iter().map(|x| x as u32).collect())
 }
 
 pub fn get_ports_by_mac(pool: web::Data<Pool>, address_mac: String) -> Result<Vec<u32>, Error> {
     let mut conn = pool.get().unwrap();
-    let mac_id = schema::mac_addresses::table
+
+    let mac_id_result: Option<i32> = schema::mac_addresses::table
         .filter(schema::mac_addresses::address_mac.eq(&address_mac))
         .select(schema::mac_addresses::id)
-        .first::<Option<i32>>(&mut conn)
-        .unwrap();
+        .first(&mut conn)
+        .optional()?;
+
+    let mac_id = match mac_id_result {
+        Some(id) => id,
+        None => return Ok(Vec::new()),
+    };
+
     let result = schema::ports::table
-        .filter(schema::ports::mac_id.eq(mac_id.unwrap()))
+        .filter(schema::ports::mac_id.eq(mac_id))
         .select(schema::ports::port)
         .load::<i32>(&mut conn)
         .unwrap();
+
     Ok(result.into_iter().map(|x| x as u32).collect())
 }
 
@@ -60,11 +68,17 @@ pub fn insert_ports(
     ports: &Vec<Port>,
 ) -> Result<(), Error> {
     let mut conn = pool.get().unwrap();
-    let mac_id = schema::mac_addresses::table
+
+    let mac_id: Option<i32> = schema::mac_addresses::table
         .filter(schema::mac_addresses::address_mac.eq(&address_mac))
         .select(schema::mac_addresses::id)
-        .first::<Option<i32>>(&mut conn)
-        .unwrap();
+        .first(&mut conn)
+        .optional()?;
+
+    if mac_id.is_none() {
+        return Err(Error::NotFound);
+    }
+
     for port in ports {
         let new_port = models::Ports {
             mac_id: mac_id.unwrap(),
@@ -73,9 +87,8 @@ pub fn insert_ports(
             created: false,
         };
         diesel::insert_into(schema::ports::table)
-            .values(new_port) // Remove the reference operator '&'
-            .execute(&mut conn)
-            .unwrap();
+            .values(new_port)
+            .execute(&mut conn)?;
     }
     Ok(())
 }
