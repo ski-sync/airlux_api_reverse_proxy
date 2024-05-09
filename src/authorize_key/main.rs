2fb859a722ef9e605d639e2c68615c57b59379af
Louis.S <louis.sasse@protonmail.com>
2024-05-09 13:07:49 +0200

    feat: create register route

--- src/authorize_key/main.rs
+++ src/authorize_key/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    print!("Hello, World!");
+use actix_web::{post, App, HttpServer};
+
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct Register {
+    pub ssh_key: String,
+}
+
+#[post("/register")]
+async fn register(register: actix_web::web::Json<Register>) -> actix_web::HttpResponse {
+    println!("{:?}", register);
+    actix_web::HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://0.0.0.0:8081");
+
+    HttpServer::new(move || App::new().service(register))
+        .bind(("0.0.0.0", 8081))?
+        .run()
+        .await
 }
