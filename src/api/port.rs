ef00b01234334eb38767c86136760b280c30b3ca
Louis.S <louis.sasse@protonmail.com>
2024-05-13 17:50:55 +0200

    feat: make function for call microservice
    add new dependencies and add new env variable

--- src/api/port.rs
+++ src/api/port.rs
@@ -1,18 +1,25 @@
 use core::fmt;
+use reqwest::blocking::Client;
+use std::env;
 use std::fmt::Display;
 
 const START_PORT: u32 = 8000;
 
-#[derive(serde::Deserialize, Debug, Clone)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Port {
     pub port: u32,
     pub protocol: Protocol,
 }
 
-#[derive(serde::Deserialize, Debug, Clone)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Register {
     pub address_mac: String,
     pub ports: Ports,
+    pub ssh_key: String,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
+pub struct RegisterAuthorizeKey {
     pub ssh_key: String,
 }
 
@@ -28,9 +35,38 @@ impl Register {
             used_ports.push(find_next_port);
         }
     }
+
+    ///
+    /// this function save ssh_key in authorized_keys file
+    /// for this he use authorize_key microservice to save ssh_key
+    ///
+    pub fn save_ssh_key(&self) {
+        let client = Client::new();
+        let url = format!(
+            "http://{}:{}/register",
+            env::var("AUTHORIZE_KEY_HOST").unwrap(),
+            env::var("AUTHORIZE_KEY_PORT").unwrap()
+        );
+
+        let response = client
+            .post(&url)
+            .json(&RegisterAuthorizeKey {
+                ssh_key: self.ssh_key.clone(),
+            })
+            .send()
+            .expect("Error sending request");
+
+        if response.status().is_success() {
+            println!("Success");
+        } else {
+            println!("Error: {:?}", response);
+            // print request body
+            println!("{:?}", response.text());
+        }
+    }
 }
 
-#[derive(serde::Deserialize, Debug, Clone)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Ports(pub Vec<Port>);
 
 impl Ports {
@@ -48,7 +84,7 @@ struct ResponseRegister {
     pub ports: Vec<u32>,
 }
 
-#[derive(serde::Deserialize, Debug, Clone)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub enum Protocol {
     Tcp,
     Http,
